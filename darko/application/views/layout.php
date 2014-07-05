466078f1ef613cce2f84aa776f70fff088468d5e
ikukic <ikukic@yahoo.com>
2014-07-05 09:01:43 +0000

    correction & add ons

--- darko/application/views/layout.php
+++ darko/application/views/layout.php
@@ -6,7 +6,7 @@
         <meta name="description" content="Accommodation - PRO" />
         <meta name="Language" content="en" />
         <title>Excursions &amp; Tours - PRO | <?=$title?></title>  
-        <link rel="icon" href="<?=base_url()?>assets/img/pngico.png" type="image/x-icon" />
+        <!--<link rel="icon" href="<?=base_url()?>assets/img/pngico.png" type="image/x-icon" />-->
 
 
 
